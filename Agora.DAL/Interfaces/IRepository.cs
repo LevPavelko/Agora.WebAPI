c3f1de485f8321e93923dc2ec16754a412178587
Nastia <nastasia.schull@gmail.com>
2025-03-08 14:17:51 +0100

    Task8  Add generic repository interface and empty repository classes.

---
+++ Agora.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agora.DAL.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IQueryable<T>> GetAll();
+        Task<T> Get(int id);
+        Task Create(T item);
+        void Update(T item);
+        Task Delete(int id);
+        
+    }
+}
