241d65586e35627c8a9bcf4801a18fd1f497ffd1
IrynaKonovalowa <iri.moummi@gmail.com>
2025-03-10 13:36:43 +0100

    Task15 Class ValidationException added to the folder Infrastructure(BLL).

---
+++ Agora.BLL/Infrastructure/ValidationException.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Agora.BLL.Infrastructure
+{
+    internal class ValidationException: Exception
+    {
+        public string Property { get; protected set; }
+        public ValidationException(string message, string prop) : base(message)
+        {
+            Property = prop;
+        }
+    }
+}
