6afebf78aa72602ae2db0a75a4a5cd6d72bd6e93
IrynaKonovalowa <iri.moummi@gmail.com>
2025-03-12 11:29:10 +0100

    Task19 OrderItemRepository, ReturnItemRepository added. ProductRepository changed (task Find added).

---
+++ Agora.DAL/Repository/OrderItemRepository.cs
@@ -0,0 +1,43 @@
+﻿using Agora.DAL.EF;
+using Agora.DAL.Entities;
+using Agora.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agora.DAL.Repository
+{
+    public class OrderItemRepository: IRepository<OrderItem>
+    {
+        private AgoraContext db;
+        public OrderItemRepository(AgoraContext context)
+        {
+            this.db = context;
+        }
+
+        public async Task<IQueryable<OrderItem>> GetAll()
+        {
+            return db.OrderItems;
+        }
+
+        public async Task<OrderItem> Get(int id)
+        {
+            return await db.OrderItems.FindAsync(id);
+        }
+
+        public async Task Create(OrderItem orderItem)
+        {
+            await db.OrderItems.AddAsync(orderItem);
+        }
+
+        public void Update(OrderItem orderItem)
+        {
+            db.Entry(orderItem).State = EntityState.Modified;
+        }
+
+        public async Task Delete(int id)
+        {
+            OrderItem? orderItem = await db.OrderItems.FindAsync(id);
+            if (orderItem != null)
+                db.OrderItems.Remove(orderItem);
+        }
+    }
+}
---
+++ Agora.DAL/Repository/ReturnItemRepository.cs
@@ -0,0 +1,43 @@
+﻿using Agora.DAL.EF;
+using Agora.DAL.Entities;
+using Agora.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agora.DAL.Repository
+{
+    public class ReturnItemRepository: IRepository<ReturnItem>
+    {
+        private AgoraContext db;
+        public ReturnItemRepository(AgoraContext context)
+        {
+            this.db = context;
+        }
+
+        public async Task<IQueryable<ReturnItem>> GetAll()
+        {
+            return db.ReturnItems;
+        }
+
+        public async Task<ReturnItem> Get(int id)
+        {
+            return await db.ReturnItems.FindAsync(id);
+        }
+
+        public async Task Create(ReturnItem returnItem)
+        {
+            await db.ReturnItems.AddAsync(returnItem);
+        }
+
+        public void Update(ReturnItem returnItem)
+        {
+            db.Entry(returnItem).State = EntityState.Modified;
+        }
+
+        public async Task Delete(int id)
+        {
+            ReturnItem? returnItem = await db.ReturnItems.FindAsync(id);
+            if (returnItem != null)
+                db.ReturnItems.Remove(returnItem);
+        }
+    }
+}
