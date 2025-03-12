20b0725eaa2faf54a53f047b0eaaeadca9a48a90
LevPavelko <lev.pavelko.studies@gmail.com>
2025-03-12 09:55:06 +0100

    Task 17: implementating of UnitOfWork

---
+++ Agora.BLL/Infrastructure/UnitOfWorkServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agora.DAL.Interfaces;
+using Agora.DAL.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Agora.BLL.Infrastructure
+{
+    public static class UnitOfWorkServiceExtensions
+    {
+        public static void AddUnitOfWorkService(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, EFUnitOfWork>();
+        }
+    }
+}
