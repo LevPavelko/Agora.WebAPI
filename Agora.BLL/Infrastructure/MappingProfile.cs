40cc2d6b0eaefd661748af44e0d6cbcfd79968c1
LevPavelko <lev.pavelko.studies@gmail.com>
2025-03-12 12:38:08 +0100

    Task 21: adding User Service and Mapping Profile

---
+++ Agora.BLL/Infrastructure/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agora.BLL.DTO;
+using Agora.DAL.Entities;
+using AutoMapper;
+
+namespace Agora.BLL.Infrastructure
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>();
+        }
+    }
+}
--- Agora.BLL/Services/UserService.cs
+++ Agora.BLL/Services/UserService.cs
@@ -1,13 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Agora.BLL.DTO;
+using Agora.BLL.Infrastructure;
 using Agora.BLL.Interfaces;
+using Agora.DAL.Entities;
+using Agora.DAL.Interfaces;
+using AutoMapper;
 
 namespace Agora.BLL.Services
 {
     public class UserService : IUserService
     {
+        IUnitOfWork Database { get; set; }
+        IMapper _mapper;
+        public UserService(IUnitOfWork uow, IMapper mapper)
+        {
+            Database = uow;
+            _mapper = mapper;
+        }
+
+        public async Task<IQueryable<UserDTO>> GetAll()
+        {
+            var users = await Database.Users.GetAll();
+            return _mapper.Map<IQueryable<UserDTO>>(users.ToList());
+
+        }
+        public async Task<UserDTO> Get(int id)
+        {
+            var user = await Database.Users.Get(id);
+            if (user == null)
+                throw new ValidationException("There is no user with this id", "");
+            return new UserDTO
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Surname = user.Surname,
+                Email = user.Email,
+                Password = user.Password
+
+            };
+        }
+
+        public async Task Create(UserDTO userDTO)
+        {
+            var user = new User
+            {
+                Name = userDTO.Name,
+                Surname = userDTO.Surname,
+                Email = userDTO.Email,
+                Password = userDTO.Password
+            };
+            await Database.Users.Create(user);
+            await Database.Save();
+        }
+        public async Task Update(UserDTO userDTO)
+        {
+
+            var user = new User
+            {
+                Id = userDTO.Id,
+                Name = userDTO.Name,
+                Surname = userDTO.Surname,
+                Email = userDTO.Email,
+                Password = userDTO.Password
+            };
+            Database.Users.Update(user);
+            await Database.Save();
+        }
+
+        public async Task Delete(int id)
+        {
+            await Database.Users.Delete(id);
+            await Database.Save();
+        }
+
     }
 }
