a4ed211088e7a8c5fbc56ee43d5f7870b7efc178
Nastia <nastasia.schull@gmail.com>
2025-04-06 17:47:41 +0200

    Task58 ProductController – Added Create, Update, Delete endpoints.

--- Agora.WebApi/Controllers/ProductController.cs
+++ Agora.WebApi/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Agora.BLL.Interfaces;
+﻿using Agora.BLL.DTO;
+using Agora.BLL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Agora.Controllers
@@ -22,6 +23,30 @@ namespace Agora.Controllers
 
             var products = await _productService.GetFilteredByName(name);
             return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] ProductDTO product)
+        {
+            await _productService.Create(product);
+            return Ok("Product created");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] ProductDTO product)
+        {
+            if (id != product.Id)
+                return BadRequest("Product ID mismatch");
+
+            await _productService.Update(product);
+            return Ok("Product updated");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _productService.Delete(id);
+            return Ok("Product deleted");
         }
     }
 }
